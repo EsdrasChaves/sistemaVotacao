aec0acc776899c396b3fbdb0a803dd62144a4b69
esdraschaves <esdraschaves1@gmail.com>
2016-12-06 17:41:07 -0200

    Terminar Consultas...

---
+++ src/classesdaoimpl/FormularioDaoImpl.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classesdaoimpl;
+
+import classesdao.FormularioDao;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author esdraschaves
+ */
+public class FormularioDaoImpl implements FormularioDao{
+    
+    
+    
+    @Override
+    public ResultSet getFormularioNumQuestao(){
+        
+    
+    
+    }
+}
